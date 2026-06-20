eefef0d8ceb22a13247af92981af8e4a880943d3
Kokare shekhar <99477907+Shekharkokare88@users.noreply.github.com>
2026-06-21 00:30:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-return()
-NNN
-
-
-@app.route('/')
 def hello_world():
     return render_template('index.html')
 
-
-@app.route('/health')
 def health():
     return 'Server is up and running'
     #thus is Python code Updated
 
-#neeeeeddddddddd
